cf8eacdffecd5297b74ad930a43f7b23be96ee34
Ruben <rubenfb14@gmail.com>
2021-09-03 14:32:05 +0200

    changed for ids aks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,12 +22,12 @@ function Robes({ Component, pageProps }) {
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@worm_emoji" />
-        <meta property="og:url" content="https://robes.market" />
-        <meta property="og:title" content="robes.market" />
+        <meta property="og:url" content="https://ak47.market" />
+        <meta property="og:title" content="ak47.market" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <meta
           property="og:description"
-          content="See the floor price of Rolls Royce from the Dope Wars project."
+          content="See the floor price of AK47s from the Dope Wars project."
         />
         <meta property="og:image" content="https://robes.market/og.png" />
         <script
